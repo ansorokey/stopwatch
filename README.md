e1f09df754a64527ac83c1c2eb16050147dd3b6b
ansorokey <ansorokey@gmail.com>
2023-11-20 17:45:21 -0500

    completed project with comments

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # stopwatch
-A stopwatch
+A simple stopwatch
 
-This project was created by following [How To Create A Stopwatch Using JavaScript | Make Stopwatch With HTML, CSS And JavaScript]()
+This project was created by following [How To Create A Stopwatch Using JavaScript | Make Stopwatch With HTML, CSS And JavaScript](https://youtu.be/cO-qjCC_UYQ?si=F_qp3MxPUS-Bmmst)
 
 ## Key Features and Technologies
+- JavaScript, HTML, CSS
+- Event Listeners
+- Asynchronous
+- Google Fonts
 
 ## How To Use
+Begin the timer by presing the triangle start button.
+Pause the timer by pressing the square stop button. \
+Resume by pressing start once again.
+Reset the timer to 0 by pressing the reset button.
 
 ## Future Features
+- Add milliseconds to timer
+- Lap button
+- Display recorded laps
+- main buton is a pause button when timer active, start button when inactive
--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let [seconds, minutes, hours] = [0, 0, 0];
+// reference html elements
 let displayTime = document.querySelector('#display-time');
+let [seconds, minutes, hours] = [0, 0, 0];
+// the timer will be an interval object when started
+// for now, initialize it as null
 let timer = null;
 
+// this is the function that increases the time by one second and displays the new time
 function stopwatch() {
     seconds += 1;
+
+    // account for time rollovers
     if(seconds === 60) {
         seconds = 0;
         minutes += 1;
@@ -13,13 +19,30 @@ function stopwatch() {
         }
     }
 
-    displayTime.innerHTML = `${hours < 10 ? '0'+hours : hours}:${minutes < 60 ? '0'+minutes : minutes}:${seconds < 60 ? '0'+seconds : seconds}`
+    // we want a two digit number, manually add in an extra 0 in front of 0-9
+    displayTime.innerHTML = `${hours < 10 ? '0'+hours : hours}:${minutes < 10 ? '0'+minutes : minutes}:${seconds < 10 ? '0'+seconds : seconds}`
 }
 
 function watchStart() {
+
+    // if there is already a timer running, we don't want two different timers active
+    // clears the existing timer OBJECT (not the time)
     if(timer !== null) {
         clearInterval(timer);
     }
 
+    // assigns the timer variable to an interval object, runs the stopwatch function every 1 second
     timer = setInterval(stopwatch, 1000);
+}
+
+// removes the interval
+function watchStop() {
+    clearInterval(timer);
+}
+
+// removes the interval AND resets variables
+function watchReset() {
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0, 0, 0];
+    displayTime.innerHTML = '00:00:00';
 }
